7a2821d5fc28c40a6dca90580d09915b044f9ad8
svey <x@svey.xyz>
2024-03-06 19:29:39 +0000

    Video cover

--- components/site/VideoBlock.tsx
+++ components/site/VideoBlock.tsx
@@ -6,7 +6,7 @@ const VideoBlock = ({source, children}:{source: string, children?:ReactElement})
 	return (
 		<div className="relative h-[900px] overflow-hidden pt-[400px] pb-[100px] items-center flex flex-col justify-center">
 			<div className="h-full w-full top-0 left-0 absolute overflow-hidden">
-				<video autoPlay loop className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-full h-auto">
+				<video autoPlay loop className="absolute min-w-full min-h-full object-cover">
 					<source src={source} type="video/mp4" />
 				</video>
 			</div>

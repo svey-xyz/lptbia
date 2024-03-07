cfcbf68c09c05ee1675cf5813351c27dbad72675
svey <x@svey.xyz>
2024-03-07 22:28:47 +0000

    Style for taxonomies

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,14 +8,14 @@ export default async function Home() {
 	return (
 		<div>
 		<VideoBlock source="https://littleportugaltorontobia.com/wp-content/uploads/2020/01/dundaswest_homevid.mp4">
-			<div className="flex flex-col md:flex-row w-full gap-x-4 gap-y-6 justify-between">
+			<div className="flex flex-row w-full gap-x-8 gap-y-12 justify-between items-center flex-wrap">
 				<a href='/' aria-label='Link to the site home.' className='relative z-10'>
 					{settings.logo &&
-						<Image image={settings.logo} size={{ width: 500, height: 200, sizes: "(max-width: 300px) 40vw, (max-width: 244px) 20vw, 20vw" }} />
+						<Image image={settings.logo} size={{ width: 450, height: 200, sizes: "(max-width: 800px) 40vw, (max-width: 800px) 20vw, 20vw" }} />
 					}
 				</a>
 				{ featuredContent.businessTaxonomies &&
-						<FeaturedTaxonomies taxonomies={featuredContent.businessTaxonomies}/>
+					<FeaturedTaxonomies taxonomies={featuredContent.businessTaxonomies}/>
 				}
 			</div>
 		</VideoBlock>
--- components/site/FeaturedTaxonomies.tsx
+++ components/site/FeaturedTaxonomies.tsx
@@ -6,21 +6,21 @@ import { Icon } from '@iconify/react';
 
 const FeaturedTaxonomies = ({ taxonomies } : { taxonomies: Array<businessTaxonomyData> }) => {
 	return (
-		<div className="relative flex flex-row flex-wrap h-auto">
+		<div className="relative flex flex-row flex-wrap">
 			{taxonomies.map((tax, i, arr) => {
 				const brightness = 100 - (i * 10)
 
 				return (
 					<a
 						key={tax._id}
-						className='relative flex flex-col gap-2 w-28 px-6 py-4 text-bg justify-center h-32 border-accent-secondary border-t-2'>
+						className='group relative flex flex-col gap-2 w-28 px-5 py-4 text-bg justify-center h-32 border-accent-secondary border-t-2'>
 						<div className='absolute inset-0 -z-1 bg-accent'
 							style={{
 								WebkitFilter: `brightness(${brightness}%)`,
 								filter: `brightness(${brightness}%)`,
 							}}/>
-						<Icon icon={tax.icon.name} width={32} className=''/>
-						<span className='text-sm font-bold'>
+						<Icon icon={tax.icon.name} width={32} className='-rotate-12 group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-300'/>
+						<span className='text-sm font-bold opacity-80 group-hover:opacity-100'>
 							{ tax.prefLabel }
 						</span>
 					</a>

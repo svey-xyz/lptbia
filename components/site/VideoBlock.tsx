ee90f37bf4770838f362b19789a7d169d6b234b0
svey <x@svey.xyz>
2024-03-12 22:29:01 +0000

    Opacity fix

--- components/site/VideoBlock.tsx
+++ components/site/VideoBlock.tsx
@@ -5,7 +5,7 @@ import { ReactElement } from 'react';
 const VideoBlock = ({source, children}:{source: string, children?:ReactElement}) => {
 	return (
 		<div className="relative h-[900px] overflow-hidden pt-[400px] pb-[100px] items-center flex flex-col justify-center
-			after:absolute after:inset-0 after:bg-bg-secondary/65">
+			after:absolute after:inset-0 after:bg-bg-secondary/[.65]">
 			<div className="h-full w-full top-0 left-0 absolute overflow-hidden">
 				<video autoPlay loop className="absolute min-w-full min-h-full object-cover">
 					<source src={source} type="video/mp4" />

d7ac4b6151bbdb32221d697e3b0856497e116cfb
svey <x@svey.xyz>
2024-09-23 14:30:42 -0400

    Project gallery

--- components/Pages/Articles/Project.tsx
+++ components/Pages/Articles/Project.tsx
@@ -3,6 +3,7 @@ import { article, article_Project } from '@/types';
 import { notFound } from 'next/navigation'
 import Image from '@components/site/Image'
 import TextBlock from '@/components/site/TextBlock';
+import Gallery from '@/components/site/Gallery';
 
 export interface ProjectPageProps {
 	data: article_Project | article
@@ -12,30 +13,55 @@ export const Project = async ({ data }: ProjectPageProps) => {
 	if (!data) return notFound();
 	if (data._type !== 'project') throw new Error(`Wrong article type - '${data._type}' - passed to Project article.)`)
 
+	const project = data as article_Project
+	
 	return (
 		<article className='py-12 max-h-fit overflow-hidden
 		'>
-			{ data.image &&
-				<div className='absolute inset-0 -m-12
-					after:-z-1 after:absolute after:inset-0 after:bg-gradient-to-t after:from-bg after:to-bg/40'>
-					<Image
-						image={data.image}
-						size={{ width: 1200, height: 1200 }}
-						style={{ objectFit: 'cover', width: '100%', height: '100%', filter: 'blur(75px)' }}
-						className='absolute inset-0 -z-1 '
-					/>
+			{project.image &&
+				<div className='absolute inset-0 -m-12'>
+					<div className='relative h-[60dvh] overflow-hidden
+						after:-z-1 after:absolute after:inset-0 after:bg-gradient-to-t after:from-bg after:via-bg/80 after:to-bg/0'>
+						<Image
+							image={project.image}
+							size={{ width: 1200, height: 1200 }}
+							style={{ objectFit: 'cover', width: '100%', height: '100%', filter: 'blur(25px)' }}
+							className='absolute inset-0 -z-1 '
+						/>
+					</div>
+					
 				</div>
 			}
-			<div className='relative z-10 flex flex-col gap-2 main-padding'>
+			<div className='relative z-10 flex flex-col gap-4 main-padding'>
 				<h1 className='text-accent text-5xl font-bold max-w-prose'>
-					{data.title}
+					{project.title}
 				</h1>
-				<p className='text-accent-secondary text-sm max-w-prose'>
-					{ data.description &&
-						<TextBlock text={data.description} />
-					}
-				</p>
 
+				{ project.description &&
+					<div className='text-sm max-w-prose-full'>
+						<TextBlock text={project.description} />
+					</div>
+				}
+
+				{ project.gallery &&
+					<div className='mt-2'>
+						<h2 className='text-accent-secondary text-2xl'>Gallery</h2>
+						<Gallery images={project.gallery} />
+					</div>
+				}
+
+				{ project.writeup &&
+					<div className='max-w-prose-full'>
+						<TextBlock text={project.writeup} />
+					</div>
+				}
+
+				{ project.credits &&
+					<div className='max-w-prose'>
+						<h2 className='text-accent-secondary'>Credits</h2>
+						<TextBlock text={project.credits} />
+					</div>
+				}
 
 				
 			</div>

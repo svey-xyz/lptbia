e464ff46e4dde52cbe7c4b8190bb5b4a4ec67c4c
svey <x@svey.xyz>
2024-09-25 16:10:18 -0400

    Hero style

--- components/blocks/Hero.tsx
+++ components/blocks/Hero.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-// import { featuredContent } from "@lib/data/data";
-
-import Image from '@/components/site/Image'
 import { block_Hero } from '@/types';
-// import urlFor from '@/lib/urlFor';
-
+import urlFor from '@/lib/urlFor';
 
 const Hero = ({ data }: { data: block_Hero | undefined }) => {
 	if (!data) return;
+	const BG_URL = data?.image ? urlFor(data?.image).url() : ''
+
 	return (
-		<div className="relative overflow-hidden flex flex-col justify-center items-center py-16 gap-12">
-			<div className="relative flex main-padding flex-col items-center">
-				<h2 className='font-bold text-accent text-3xl'>
+		<div
+			className={`relative overflow-hidden flex flex-col justify-center bg-fixed bg-no-repeat bg-cover bg-center text-accent
+				after:absolute after:inset-0 after:bg-gradient-to-t after:from-bg after:to-bg/0`}
+			style={{
+				backgroundImage: `url(${BG_URL})`
+			}}
+		>
+			<div className="relative flex main-padding flex-col items-center pt-80 pb-6 z-10">
+				<h2 className='font-black text-accent text-4xl'>
 					{data.title}
 				</h2>
 			</div>
--- components/sections/Standard.tsx
+++ components/sections/Standard.tsx
@@ -1,8 +1,14 @@
+import { section } from '@/types';
 import React, { ReactNode } from 'react';
 
-export const Standard = ({children, index}:{children:ReactNode, index: number}) => {
+export const Standard = ({ data, children, index }: { data?: section, children:ReactNode, index: number}) => {
+	const hasHero = data?.blocks ? data.blocks[0]._type == 'Hero' : false
+	
 	return (
-		<div>
+		<div
+			className={`relative overflow-hidden flex flex-col justify-center
+			${(index == 0 && hasHero) ? '-mt-[--total-header-height]' : ''}`}
+		>
 			{ children }
 		</div>
 	);

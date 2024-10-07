8ec30627906ca8835408fe68c70dd59fc2813976
svey <x@svey.xyz>
2024-10-07 15:53:14 -0400

    Remove depreceated columns block

--- sanity/schemas/pages/blocks/index.ts
+++ sanity/schemas/pages/blocks/index.ts
@@ -7,18 +7,10 @@ import { Info } from './Info';
 import { Hero } from './Hero';
 import { Archive } from './Archive'
 import { People } from './People'
-import { Columns } from './Columns'
 import { Image } from './Image'
 import { Contact } from './Contact'
 
-export const BlocksWithoutColumns = [FeaturedTaxonomies, Text, Map, FeaturedArticles, Newsletter, Info, Archive, People, Image, Contact]
-
-export const blockTypesWithoutColumns = BlocksWithoutColumns.map((block) => {
-	return { type: block.name }
-})
-
-
-export const Blocks = [FeaturedTaxonomies, Text, Map, FeaturedArticles, Newsletter, Info, Archive, Hero, People, Columns, Image, Contact];
+export const Blocks = [FeaturedTaxonomies, Text, Map, FeaturedArticles, Newsletter, Info, Archive, Hero, People, Image, Contact];
 
 export const blockTypes = Blocks.map((block) => {
 	return { type: block.name }

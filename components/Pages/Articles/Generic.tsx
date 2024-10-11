0e53fb2460444b98946b9d35099a068f9ad26b95
svey <x@svey.xyz>
2024-10-11 16:23:18 -0400

    Basic news article page

--- components/Pages/Articles/Generic.tsx
+++ components/Pages/Articles/Generic.tsx
@@ -9,8 +9,12 @@ export interface GenericArticlePageProps {
 export const Generic = async ({ data }: GenericArticlePageProps) => {
 	if (!data) return notFound();
 	return (
-		<article className=''>
-			<h1>{data.title}</h1>
+		<article className='py-12 max-h-fit overflow-hidden'>
+			<div className='relative z-10 flex flex-col gap-4 main-padding'>
+				<h1 className='text-accent text-5xl font-bold max-w-prose'>
+					{ data.title }
+				</h1>
+			</div>
 		</article>
 	);
 };

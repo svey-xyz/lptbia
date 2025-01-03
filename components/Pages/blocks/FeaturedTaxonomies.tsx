12098ad7942b6e802870e99fef16c6553555da99
svey-xyz <x@svey.xyz>
2025-01-03 13:08:44 -0500

    Featured Taxonomie Style Push featrued taxonomies to bottom right of section, and resize.

--- components/Pages/blocks/FeaturedTaxonomies.tsx
+++ components/Pages/blocks/FeaturedTaxonomies.tsx
@@ -10,7 +10,7 @@ import { taxTypeToArticleType } from '@/lib/taxTypeToArticleType';
 export const FeaturedTaxonomies = ({ data, className } : { data: block_FeaturedTaxonomies | undefined, className?: string } ) => {
 	if (!data) return;
 	return (
-		<div className="flex flex-row flex-wrap justify-center w-full h-4/6 gap-y-4">
+		<div className="absolute flex flex-row flex-wrap justify-end w-auto h-auto gap-y-2 bottom-0 right-0">
 			{data.taxonomies?.map((tax, i, arr) => {
 				const brightness = 100 - (i * 10)
 
@@ -18,14 +18,14 @@ export const FeaturedTaxonomies = ({ data, className } : { data: block_FeaturedT
 					<a
 						key={tax._id}
 						href={`${resolveArchiveHref(taxTypeToArticleType(tax))}?filter=${encodeURIComponent(tax.prefLabel)}`} 
-						className='group relative flex flex-col gap-4 flex-grow px-5 pt-4 lg:pb-16 pb-8 text-bg justify-end border-transparent border-t-2 hover:border-accent-secondary'>
+						className='group relative flex flex-row flex-wrap gap-8 px-8 py-4 text-bg justify-end border-transparent border-t-2 hover:border-accent-secondary'>
 						<div className='absolute inset-0 -z-1 bg-accent/75 saturate-150'
 							style={{
 								WebkitFilter: `brightness(${brightness}%)`,
 								filter: `brightness(${brightness}%)`,
 							}}/>
 						{ tax.icon?.name &&
-							<Icon icon={tax.icon?.name} width={60} className='-rotate-12 group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-300'/>
+							<Icon icon={tax.icon?.name} width={40} className='-rotate-12 group-hover:-rotate-6 group-hover:scale-110 transition-transform duration-300'/>
 						}
 						<span className='text-lg font-bold opacity-80 group-hover:opacity-100 leading-tight' style={{ whiteSpace: "pre-line" }}>
 							{ breakAdditionSign(tax.prefLabel) }

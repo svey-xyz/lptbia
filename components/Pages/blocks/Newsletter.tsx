343b4eb6ee3ee82a16e914b5cfec77dbab021b81
svey-xyz <x@svey.xyz>
2025-01-31 12:49:03 -0500

    Fix newsletter form width on mobile

--- components/Pages/blocks/Newsletter.tsx
+++ components/Pages/blocks/Newsletter.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 
 export const Standard = ({ data, className }: { data: block_Newsletter, className?: string }) => {
 	return (
-		<div className={`${className} w-prose-full`}>
+		<div className={`${className} w-prose-full max-w-full`}>
 			<h3>
 				{ data.callToAction }
 			</h3>
 			<span>
 				{ data.text }
 			</span>
-			<NewsletterForm stacked={true} className="max-w-prose mx-auto" uuid={`${data._key}`} audienceID={data.audienceID} />
+			<NewsletterForm stacked={true} className="w-full max-w-prose mx-auto" uuid={`${data._key}`} audienceID={data.audienceID} />
 		</div>
 	);
 };
